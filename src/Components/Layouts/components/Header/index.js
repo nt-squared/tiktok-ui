7c9b1176b5d1275a022980e1b80b380f84371e91
nt-squared <ngoctamnguyen3147@gmail.com>
2023-03-03 06:41:59 +0700

    F8 ReactJS bai 97

--- src/Components/Layouts/components/Header/index.js
+++ src/Components/Layouts/components/Header/index.js
@@ -1,5 +1,9 @@
 import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 function Header() {
     const inner = clsx('container', styles.inner);
@@ -7,7 +11,35 @@ function Header() {
         <header className={styles.wrapper}>
             <div className={inner}>
                 {/* Logo */}
+                <div className={styles.logoContainer}>
+                    <Link to="/" className={styles.logo}>
+                        <img src={images.logo} alt="Tiktok" />
+                    </Link>
+                </div>
+
                 {/* Search Bar */}
+                <div className={styles.searchWrapper}>
+                    <div className={styles.searchContainer}>
+                        <input
+                            type="search"
+                            placeholder="Search accounts and videos"
+                            spellCheck={false}
+                            className={styles.searchInput}
+                        />
+                        <button className={styles.clearIcon}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        {/* <button>
+                            <FontAwesomeIcon icon={faSpinner} />
+                        </button> */}
+                        <span></span>
+                        <button type="submit" className={styles.searchIcon}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </div>
+
+                <div>{/* action field */}</div>
             </div>
         </header>
     );
