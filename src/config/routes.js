0b069295d860695aed22d6bcf3354c2cc4cdc528
nt-squared <ngoctamnguyen3147@gmail.com>
2023-03-09 07:58:17 +0700

    update routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,10 @@
 const routes = {
-    home: '/',
-    following: '/following',
-    profile: '/:nickname',
-    upload: '/upload',
-    search: '/search',
-    live: '/live',
+    home: '/tiktok-ui',
+    following: '/tiktok-ui/following',
+    profile: '/tiktok-ui/:nickname',
+    upload: '/tiktok-ui/upload',
+    search: '/tiktok-ui/search',
+    live: '/tiktok-ui/live',
 };
 
 export default routes;
